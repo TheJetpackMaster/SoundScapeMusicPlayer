fed4360b901e975f7b66f60b5c66ee420d420f6c
hP <balochkhubaib008@gmail.com>
2024-05-24 19:28:29 +0500

    add features in video playing screen.

--- app/src/main/java/com/SoundScapeApp/soundscape/PlayerActivity.kt
+++ app/src/main/java/com/SoundScapeApp/soundscape/PlayerActivity.kt
@@ -110,7 +110,7 @@ class PlayerActivity : ComponentActivity() {
         if (videoUri != null) {
             videoViewModel.onNewIntent(videoUri)
         }
-        val parvezname = 5;
+        val name = 5;
     }
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
