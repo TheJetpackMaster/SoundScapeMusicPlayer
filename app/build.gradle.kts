33e14e1de3e01f1cbcd464d3aee2dd981d63aa7d
ParvezMayar <parvezbaloc32@gmail.com>
2024-06-15 11:03:20 +0500

    version 1.3.0 ,with many changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.SoundScapeApp.soundscape"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.2.3"
+        versionCode = 7
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
